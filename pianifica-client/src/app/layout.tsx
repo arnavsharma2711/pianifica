8e7c7d701827ea9e3f7d14a204849ad2b990ff66
Arnav Sharma <arnavsharma2711@gmail.com>
2024-12-01 15:20:50 +0530

    feat(client): Implement DashboardWrapper component to structure layout with Navbar and Sidebar

--- pianifica-client/src/app/layout.tsx
+++ pianifica-client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
+import DashboardWrapper from "./dashboard-wrapper";
 
 const raleway = Raleway({
 	subsets: ["latin"],
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${raleway.className} antialiased`}>{children}</body>
+			<body className={`${raleway.className} antialiased`}>
+				<DashboardWrapper>{children}</DashboardWrapper>
+			</body>
 		</html>
 	);
 }
