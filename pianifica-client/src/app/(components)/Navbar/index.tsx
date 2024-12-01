8284cb6bb81487dcef504d74ddfe768ef72c837d
Arnav Sharma <arnavsharma2711@gmail.com>
2024-12-01 16:08:03 +0530

    feat(client): Enhance Navbar with dark mode toggle and sidebar collapse functionality; update DashboardWrapper for state management

--- pianifica-client/src/app/(components)/Navbar/index.tsx
+++ pianifica-client/src/app/(components)/Navbar/index.tsx
@@ -1,10 +1,25 @@
-import { Search, Settings } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Menu, Moon, Search, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 const Navbar = () => {
+	const dispatch = useAppDispatch();
+	const isSidebarCollapsed = useAppSelector(
+		(state) => state.global.isSidebarCollapsed,
+	);
+	const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 	return (
 		<div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
 			{/* Search Bar */}
 			<div className="flex items-center gap-8">
+				{isSidebarCollapsed ? null : (
+					<button
+						type="button"
+						onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+					>
+						<Menu className="h-8 w-8 cursor-pointer dark:text-white" />
+					</button>
+				)}
 				<div className="relative flex h-min w-[200px]">
 					<Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
 					<input
@@ -16,9 +31,20 @@ const Navbar = () => {
 			</div>
 			{/* Navbar Items */}
 			<div className="flex items-center">
+				<button
+					type="button"
+					onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+					className={`rounded p-2 ${isDarkMode ? "dark:hover:bg-gray-700" : "hover:bg-gray-200"}`}
+				>
+					{isDarkMode ? (
+						<Sun className="h-6 w-6 cursor-pointer dark:text-white" />
+					) : (
+						<Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+					)}
+				</button>
 				<Link
 					href="settings"
-					className="h-min w-min rounded p-2 hover:bg-gray-200"
+					className={`h-min w-min rounded p-2 ${isDarkMode ? "dark:hover:bg-gray-700" : "hover:bg-gray-200"}`}
 				>
 					<Settings className="h-6 w-6 cursor-pointer dark:text-white" />
 				</Link>
