990097fe0ed7597167eaa8bf510ca79d26293f26
Arnav Sharma <arnavsharma2711@gmail.com>
2024-12-08 01:31:20 +0530

    feat(client): Add Project page with BoardView and ProjectHeader components

---
+++ pianifica-client/src/app/project/[id]/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Board from "@/components/Pages/Project/BoardView";
+import ProjectHeader from "@/components/Pages/Project/ProjectHeader";
+import React, { useState, useEffect } from "react";
+
+type Props = {
+	params: Promise<{ id: string }>;
+};
+
+const Project = ({ params }: Props) => {
+	const [id, setId] = useState<string | null>(null);
+	const [activeTab, setActiveTab] = useState("Board");
+	const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
+	useEffect(() => {
+		params.then((resolvedParams) => {
+			setId(resolvedParams.id);
+		});
+	}, [params]);
+
+	if (!id) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<div>
+			<ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+			{activeTab === "Board" && (
+				<Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+			)}
+		</div>
+	);
+};
+
+export default Project;
