04d153af7b3c953f0db28923233db150caf6631d
Arnav Sharma <arnavsharma2711@gmail.com>
2024-12-08 04:25:47 +0530

    fix(client): Update SidebarLink styles for improved dark mode support

--- pianifica-client/src/components/Sidebar/index.tsx
+++ pianifica-client/src/components/Sidebar/index.tsx
@@ -39,7 +39,7 @@ const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkProps) => {
 	return (
 		<Link href={href} className="w-full">
 			<div
-				className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""} justify-start px-8 py-3`}
+				className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""} justify-start px-8 py-3`}
 			>
 				{isActive && (
 					<div className="absolute left-0 top-0 h-full w-[5px] bg-blue-200" />
