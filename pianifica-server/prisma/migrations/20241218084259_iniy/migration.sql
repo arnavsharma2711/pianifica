1e027f7bef54f4d3aacba277593df74d5db2f375
Arnav Sharma <arnavsharma2711@gmail.com>
2024-12-18 14:30:50 +0530

    chore(server): Update seed data and dependencies; remove unused seed files

---
+++ pianifica-server/prisma/migrations/20241218084259_iniy/migration.sql
@@ -0,0 +1,183 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('BLOCKED', 'TODO', 'IN_PROGRESS', 'UNDER_REVIEW', 'RELEASE_READY', 'COMPLETED');
+
+-- CreateEnum
+CREATE TYPE "Priority" AS ENUM ('BACKLOG', 'LOW', 'MEDIUM', 'HIGH', 'URGENT');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "organizationId" INTEGER NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "profilePictureUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserRole" (
+    "userId" INTEGER NOT NULL,
+    "roleId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserRole_pkey" PRIMARY KEY ("userId","roleId")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "leadId" INTEGER,
+    "managerId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Organization" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "organizationId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "startDate" TIMESTAMP(3),
+    "endDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectTeam" (
+    "id" SERIAL NOT NULL,
+    "teamId" INTEGER NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ProjectTeam_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" "Status",
+    "priority" "Priority",
+    "tags" TEXT,
+    "startDate" TIMESTAMP(3),
+    "dueDate" TIMESTAMP(3),
+    "points" INTEGER,
+    "projectId" INTEGER NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "assigneeId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Attachment" (
+    "id" SERIAL NOT NULL,
+    "fileUrl" TEXT NOT NULL,
+    "fileName" TEXT,
+    "taskId" INTEGER NOT NULL,
+    "uploadedById" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Attachment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT NOT NULL,
+    "taskId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectTeam" ADD CONSTRAINT "ProjectTeam_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectTeam" ADD CONSTRAINT "ProjectTeam_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_assigneeId_fkey" FOREIGN KEY ("assigneeId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attachment" ADD CONSTRAINT "Attachment_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attachment" ADD CONSTRAINT "Attachment_uploadedById_fkey" FOREIGN KEY ("uploadedById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- pianifica-server/prisma/seed.ts
+++ pianifica-server/prisma/seed.ts
@@ -1,19 +1,35 @@
 import { PrismaClient } from "@prisma/client";
-import fs from "node:fs";
-import path from "node:path";
 const prisma = new PrismaClient();
+import { faker } from "@faker-js/faker";
 
-async function deleteAllData(orderedFileNames: string[]) {
-  const modelNames = orderedFileNames.map((fileName) => {
-    const modelName = path.basename(fileName, path.extname(fileName));
-    return modelName.charAt(0).toUpperCase() + modelName.slice(1);
-  });
+async function createData(model: any, data: object) {
+  try {
+    return await model.create({ data });
+  } catch (error) {
+    console.error(`Error seeding data for ${model}:`, error);
+  }
+}
 
+async function deleteAllData() {
+  const modelNames = [
+    "Comment",
+    "Attachment",
+    "Task",
+    "ProjectTeam",
+    "Team",
+    "Project",
+    "UserRole",
+    "Role",
+    "User",
+    "Organization",
+  ];
   for (const modelName of modelNames) {
-    const model: any = prisma[modelName as keyof typeof prisma];
     try {
-      await model.deleteMany({});
-      console.log(`Cleared data from ${modelName}`);
+      await prisma.$executeRawUnsafe(`DELETE FROM "${modelName}";`);
+      await prisma.$executeRawUnsafe(
+        `ALTER SEQUENCE "${modelName}_id_seq" RESTART WITH 1;`
+      );
+      console.log(`Cleared data from ${modelName} and reset ID sequence`);
     } catch (error) {
       console.error(`Error clearing data from ${modelName}:`, error);
     }
@@ -21,36 +37,290 @@ async function deleteAllData(orderedFileNames: string[]) {
 }
 
 async function main() {
-  const dataDirectory = path.join(__dirname, "seedData");
+  await deleteAllData();
 
-  const orderedFileNames = [
-    "team.json",
-    "project.json",
-    "projectTeam.json",
-    "user.json",
-    "task.json",
-    "attachment.json",
-    "comment.json",
-    "taskAssignment.json",
-  ];
+  await createOrganization();
+  const orgUsers = await createUser();
+  await createRole();
+  const admins = await createUserRole();
+  await createProject();
+  await createTeam(admins);
+  await createTask(admins, orgUsers);
+}
 
-  await deleteAllData(orderedFileNames);
+const OrganizationModel: any = prisma["Organization" as keyof typeof prisma];
+const UserModel: any = prisma["User" as keyof typeof prisma];
+const RoleModel: any = prisma["Role" as keyof typeof prisma];
+const UserRoleModel: any = prisma["UserRole" as keyof typeof prisma];
+const ProjectModel: any = prisma["Project" as keyof typeof prisma];
+const TeamModel: any = prisma["Team" as keyof typeof prisma];
+const ProjectTeamModel: any = prisma["ProjectTeam" as keyof typeof prisma];
+const TaskModel: any = prisma["Task" as keyof typeof prisma];
+const AttachmentModel: any = prisma["Attachment" as keyof typeof prisma];
+const CommentModel: any = prisma["Comment" as keyof typeof prisma];
 
-  for (const fileName of orderedFileNames) {
-    const filePath = path.join(dataDirectory, fileName);
-    const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    const modelName = path.basename(fileName, path.extname(fileName));
-    const model: any = prisma[modelName as keyof typeof prisma];
+async function createOrganization() {
+  console.log("Creating organizations...");
 
-    try {
-      for (const data of jsonData) {
-        await model.create({ data });
+  await createData(OrganizationModel, { name: "Pianifica" });
+
+  const organizationData = Array.from({ length: 5 }, () => ({
+    name: faker.company.name(),
+  }));
+
+  for (const data of organizationData) {
+    await createData(OrganizationModel, data);
+  }
+  console.log("Organizations created successfully!");
+}
+
+async function createUser() {
+  const organizations = await OrganizationModel.findMany();
+  console.log("Creating users...");
+
+  await createData(UserModel, {
+    organizationId: 1,
+    firstName: "Arnav",
+    lastName: "Sharma",
+    username: "arnavsharma2711",
+    email: "arnavsharma2711@gmail.com",
+    password: "password",
+    profilePictureUrl: faker.image.avatar(),
+  });
+
+  const orgUsers: { [key: number]: number[] } = {};
+
+  for (const organization of organizations) {
+    const userData = Array.from({ length: 5 }, () => {
+      const firstName = faker.person.firstName();
+      const lastName = faker.person.lastName();
+      const username = faker.internet.username({
+        firstName: firstName,
+        lastName: lastName,
+      });
+      const email = faker.internet.email({
+        firstName: firstName,
+        lastName: lastName,
+      });
+      return {
+        organizationId: organization.id,
+        firstName: firstName,
+        lastName: lastName,
+        username: username.toLowerCase(),
+        email: email.toLowerCase(),
+        password: faker.internet.password(),
+        profilePictureUrl: faker.image.avatar(),
+      };
+    });
+
+    for (const data of userData) {
+      if (!orgUsers[organization.id]) {
+        orgUsers[organization.id] = [];
       }
-      console.log(`Seeded ${modelName} with data from ${fileName}`);
-    } catch (error) {
-      console.error(`Error seeding data for ${modelName}:`, error);
+      const createdUser = await createData(UserModel, data);
+      orgUsers[organization.id].push(createdUser.id);
     }
   }
+  console.log("Users created successfully!");
+
+  return orgUsers;
+}
+
+async function createRole() {
+  console.log("Creating roles...");
+  const roleData = [
+    {
+      name: "Super Admin",
+      description: "The user has access to all privileges",
+    },
+    {
+      name: "Organization Admin",
+      description:
+        "The user has access to all privileges within the organization",
+    },
+  ];
+
+  for (const data of roleData) {
+    await createData(RoleModel, data);
+  }
+  console.log("Roles created successfully!");
+}
+
+async function createUserRole() {
+  console.log("Creating user roles...");
+
+  await createData(UserRoleModel, {
+    userId: 1,
+    roleId: 1,
+  });
+
+  const users = await UserModel.findMany({
+    distinct: ["organizationId"],
+  });
+
+  const admins: { [key: number]: number } = {};
+
+  for (const user of users) {
+    const organizationId = user.organizationId;
+    const userId = user.id;
+    admins[organizationId] = userId;
+    const userRole = {
+      userId: userId,
+      roleId: 2,
+    };
+    await createData(UserRoleModel, userRole);
+  }
+
+  console.log("User roles created successfully!");
+
+  return admins;
+}
+
+async function createProject() {
+  console.log("Creating projects...");
+
+  const organizations = await OrganizationModel.findMany({
+    where: { id: { not: 1 } },
+  });
+
+  for (const organization of organizations) {
+    const projects = Array.from({ length: 5 }, () => ({
+      name: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+      organizationId: organization.id,
+      startDate: faker.date.past(),
+      endDate: faker.date.future(),
+    }));
+
+    for (const project of projects) {
+      await createData(ProjectModel, project);
+    }
+  }
+
+  console.log("Projects created successfully!");
+}
+
+async function createTeam(admins: { [key: number]: number }) {
+  console.log("Creating teams and project teams...");
+
+  const users = await UserModel.findMany({
+    distinct: ["organizationId"],
+    where: {
+      organizationId: {
+        not: 1,
+      },
+      id: {
+        notIn: Object.values(admins),
+      },
+    },
+  });
+
+  for (const user of users) {
+    const teams = Array.from({ length: 1 }, () => ({
+      name: faker.commerce.department(),
+      managerId: admins[user.organizationId],
+      leadId: user.id,
+    }));
+
+    for (const team of teams) {
+      const createdTeam = await createData(TeamModel, team);
+      const projects = await ProjectModel.findMany({
+        where: { organizationId: user.organizationId },
+      });
+      for (const project of projects) {
+        await createData(ProjectTeamModel, {
+          teamId: createdTeam.id,
+          projectId: project.id,
+        });
+      }
+    }
+  }
+
+  console.log("Teams and project teams created successfully!");
+}
+
+async function createTask(
+  admins: { [key: number]: number },
+  orgUsers: { [key: number]: number[] }
+) {
+  enum Status {
+    BLOCKED = "BLOCKED",
+    TODO = "TODO",
+    IN_PROGRESS = "IN_PROGRESS",
+    UNDER_REVIEW = "UNDER_REVIEW",
+    RELEASE_READY = "RELEASE_READY",
+    COMPLETED = "COMPLETED",
+  }
+
+  enum Priority {
+    BACKLOG = "BACKLOG",
+    LOW = "LOW",
+    MEDIUM = "MEDIUM",
+    HIGH = "HIGH",
+    URGENT = "URGENT",
+  }
+
+  function getRandomEnumValue<T extends object>(enumObj: T): T[keyof T] {
+    const enumValues = Object.values(enumObj) as T[keyof T][];
+    const randomIndex = Math.floor(Math.random() * enumValues.length);
+    return enumValues[randomIndex];
+  }
+
+  console.log("Creating tasks, comments and attachments...");
+
+  const projects = await ProjectModel.findMany();
+
+  for (const project of projects) {
+    const tasks = [];
+    for (const user of orgUsers[project.organizationId]) {
+      tasks.push({
+        title: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        status: getRandomEnumValue(Status),
+        priority: getRandomEnumValue(Priority),
+        tags: "",
+        points: Math.floor(Math.random() * 5),
+        projectId: project.id,
+        authorId: admins[project.organizationId],
+        assigneeId: user,
+        startDate: faker.date.past(),
+        dueDate: faker.date.future(),
+      });
+
+      for (const task of tasks) {
+        const createdTask = await createData(TaskModel, task);
+
+        const comments = Array.from(
+          { length: Math.floor(Math.random() * 4) },
+          () => ({
+            text: faker.lorem.sentence(),
+            taskId: createdTask.id,
+            userId:
+              orgUsers[project.organizationId][
+                Math.floor(
+                  Math.random() * orgUsers[project.organizationId].length
+                )
+              ],
+          })
+        );
+
+        for (const comment of comments) {
+          await createData(CommentModel, comment);
+        }
+
+        if (Math.random() < 0.1) {
+          await createData(AttachmentModel, {
+            taskId: createdTask.id,
+            uploadedById: createdTask.authorId,
+            fileUrl: faker.image.url(),
+            fileName: faker.system.commonFileName("jpg"),
+          });
+        }
+      }
+    }
+  }
+
+  console.log("Tasks, comments and attachments created successfully!");
 }
 
 main()
