2dd57023834fde3e44adb6bbf7409224edccb83b
Arnav Sharma <arnavsharma2711@gmail.com>
2024-12-18 22:17:39 +0530

    feat(server): Add user routes and controller to retrieve active users

---
+++ pianifica-server/src/controllers/user-controller.ts
@@ -0,0 +1,18 @@
+import type { Request, Response } from "express";
+import prisma from "../utils/prisma";
+
+export const getUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const users = await prisma.user.findMany({
+      where: { deletedAt: null },
+    });
+    res.json(users);
+  } catch (error) {
+    if (error instanceof Error) {
+      res
+        .status(500)
+        .json({ message: `Error retrieving users: ${error.message}` });
+    }
+    res.status(500).json({ message: "An unknown error occurred" });
+  }
+};
--- pianifica-server/src/routes.ts
+++ pianifica-server/src/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import projectRoutes from "./routes/project-routes";
 import taskRoutes from "./routes/task-routes";
+import userRoutes from "./routes/user-routes";
 import searchRoutes from "./routes/search-routes";
 
 const router = Router();
 
 router.use("/project", projectRoutes);
 router.use("/task", taskRoutes);
+router.use("/users", userRoutes);
 router.use("/search", searchRoutes);
 
 export default router;
