b0e466fe715ee0d20ee6956bbc3fd00c37102564
Arnav Sharma <arnavsharma2711@gmail.com>
2024-12-20 01:44:47 +0530

    feat(server): Enhance search functionality with case-insensitive queries and limit results

--- pianifica-server/src/controllers/search-controller.ts
+++ pianifica-server/src/controllers/search-controller.ts
@@ -4,44 +4,77 @@ import prisma from "../utils/prisma";
 export const search = async (req: Request, res: Response): Promise<void> => {
   try {
     const q = req.query.q as string;
+    const limit = req.query.limit ? Number(req.query.limit) : 10;
     if (!q) {
       res.status(400).json({ message: "Query parameter 'q' is required" });
       return;
     }
 
-    const task = await prisma.task.findMany({
+    const tasks = await prisma.task.findMany({
       where: {
         AND: [
           { deletedAt: null },
           {
-            OR: [{ title: { contains: q } }, { description: { contains: q } }],
+            OR: [
+              { title: { contains: q, mode: "insensitive" } },
+              { description: { contains: q, mode: "insensitive" } },
+            ],
           },
         ],
       },
+      orderBy: {
+        updatedAt: "desc",
+      },
+      take: limit,
     });
+
     const projects = await prisma.project.findMany({
       where: {
         AND: [
           { deletedAt: null },
           {
-            OR: [{ name: { contains: q } }, { description: { contains: q } }],
+            OR: [
+              { name: { contains: q, mode: "insensitive" } },
+              { description: { contains: q, mode: "insensitive" } },
+            ],
           },
         ],
       },
-    });
-    const users = await prisma.user.findMany({
-      where: {
-        AND: [{ deletedAt: null }, { username: { contains: q } }],
+      orderBy: {
+        updatedAt: "desc",
       },
+      take: limit,
     });
 
-    res.json({ task, projects, users });
+    const users = await prisma.user.findMany({
+      where: {
+        AND: [
+          { deletedAt: null },
+
+          {
+            OR: [
+              { username: { contains: q, mode: "insensitive" } },
+              { firstName: { contains: q, mode: "insensitive" } },
+              { lastName: { contains: q, mode: "insensitive" } },
+              { email: { contains: q, mode: "insensitive" } },
+            ],
+          },
+        ],
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+      take: limit,
+    });
+
+    res.json({ tasks, projects, users });
   } catch (error) {
     if (error instanceof Error) {
       res
         .status(500)
         .json({ message: `Error retrieving projects: ${error.message}` });
+    } else {
+      res.status(500).json({ message: "An unknown error occurred" });
     }
-    res.status(500).json({ message: "An unknown error occurred" });
   }
 };
