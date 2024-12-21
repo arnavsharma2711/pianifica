c56aab985a00b662e90b43f0aa7ede4141d0b4d6
Arnav Sharma <arnavsharma2711@gmail.com>
2024-12-21 14:32:42 +0530

    feat(server): Set up Express server with middleware and error handling

---
+++ pianifica-server/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import morganLogger from "morgan";
+import helmetSecurity from "helmet";
+import corsMiddleware from "cors";
+import bodyParser from "body-parser";
+import cookieParserMiddleware from "cookie-parser";
+import path from "node:path";
+
+import { handleError, handleNotFound } from "./middlewares/middlewares";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const expressApp = express();
+expressApp.use(morganLogger("dev"));
+expressApp.use(helmetSecurity());
+expressApp.use(corsMiddleware());
+expressApp.use(bodyParser.json());
+expressApp.use(express.json());
+expressApp.use(cookieParserMiddleware());
+
+// Define root route
+expressApp.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+// Importing API routes
+import routes from "./routes";
+expressApp.use("/api", routes);
+
+// Use custom middlewares for handling 404 and errors
+expressApp.use(handleNotFound);
+expressApp.use(handleError);
+
+export default expressApp;
--- pianifica-server/src/index.ts
+++ pianifica-server/src/index.ts
@@ -1,36 +1,9 @@
-import express from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
-/* ROUTE IMPORTS */
-import routes from "./routes";
+import app from "./app";
+import { PORT } from "./constants";
 
-/* CONFIGURATIONS */
-dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-
-/* ROUTES */
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/health", (req, res) => {
-  res.send("Server is up and running");
-});
-
-app.use("/api", routes);
-
-/* SERVER */
-const port = Number(process.env.PORT) || 3000;
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server running on part ${port}`);
+const serverPort = PORT || 8000;
+app.listen(serverPort, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${serverPort}`);
+  /* eslint-enable no-console */
 });
