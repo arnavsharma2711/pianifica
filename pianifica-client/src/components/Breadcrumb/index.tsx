b020b695e894b843888645c6ff0db7c7bb51b56c
Arnav Sharma <arnavsharma2711@gmail.co>
2024-12-29 01:04:16 +0530

    feat(client): Update Breadcrumb component to handle disabled links with appropriate styling

--- pianifica-client/src/components/Breadcrumb/index.tsx
+++ pianifica-client/src/components/Breadcrumb/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Breadcrumb = ({ links }: { links: { value: string; link: string; disable?: boolean }[] }) => {
   return (
-    <nav className="flex p-4" aria-label="Breadcrumb">
+    <nav className="flex p-4 whitespace-nowrap overflow-x-auto" aria-label="Breadcrumb">
       <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
         <li className="inline-flex items-center">
           <Link href="/" className="inline-flex items-center gap-2 text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
@@ -14,12 +14,19 @@ const Breadcrumb = ({ links }: { links: { value: string; link: string; disable?:
         {links.map((link) => (
           <li key={link.value} className="inline-flex items-center">
             <ChevronRight size={15} className="text-gray-400 dark:text-gray-400" />
-            {!link?.disable && <Link href={link.link} className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">{link.value}</Link>}
+            {link?.disable ? (
+              <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">{link.value}</span>
+            ) : (
+              <Link href={link.link} className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
+                {link.value}
+              </Link>
+            )}
           </li>
         ))}
       </ol>
     </nav>
   );
 };
+
 
 export default Breadcrumb;
